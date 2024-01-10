51526da7cb92efcee2ae50abb037f071f654c088
ruli sastra putri <ruli@accelbyte.net>
2024-01-10 14:55:40 +0700

    ci(nightly): delete the retry attempt to ping 8080

--- test/functional/test-local-hosted.sh
+++ test/functional/test-local-hosted.sh
@@ -17,9 +17,6 @@ echo '# Build and run Extend app locally'
 python -m pip install -r requirements.txt
 (cd src && python -m app)& GRPC_SERVER_PID=$!
 
-echo '# Run'
-(for _ in {1..12}; do bash -c "timeout 1 echo > /dev/tcp/127.0.0.1/8080" 2>/dev/null && exit 0 || sleep 5s; done; exit 1)
-
 if [ $? -ne 0 ]; then
   echo "Failed to run Extend app locally"
   exit 1
@@ -57,4 +54,3 @@ fi
 echo '# Testing Extend app using demo script'
 
 GRPC_SERVER_URL="http://${NGROK_URL#*://}" bash demo.sh
-
