8b9aef34e78234559c8ce4b54fe68c5b395cbb9e
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2024-06-12 19:32:42 +0700

    test: improve demo.sh

--- demo.sh
+++ demo.sh
@@ -7,88 +7,90 @@
 set -e
 set -o pipefail
 
-test -n "$GRPC_SERVER_URL" || (echo "GRPC_SERVER_URL is not set"; exit 1)
 test -n "$AB_CLIENT_ID" || (echo "AB_CLIENT_ID is not set"; exit 1)
 test -n "$AB_CLIENT_SECRET" || (echo "AB_CLIENT_SECRET is not set"; exit 1)
 test -n "$AB_NAMESPACE" || (echo "AB_NAMESPACE is not set"; exit 1)
 
-get_code_verifier()
+if [ -z "$GRPC_SERVER_URL" ] && [ -z "$EXTEND_APP_NAME" ]; then
+  echo "GRPC_SERVER_URL or EXTEND_APP_NAME is not set"
+  exit 1
+fi
+
+DEMO_PREFIX='cloudsave_grpc_demo'
+
+api_curl()
 {
-  echo $RANDOM | sha256sum | cut -d ' ' -f 1   # For demo only: In reality, it needs to be secure random
-}
-
-get_code_challenge()
-{
-  echo -n "$1" | sha256sum | xxd -r -p | base64 | tr -d '\n' | sed -e 's/\+/-/g' -e 's/\//\_/g' -e 's/=//g'
-}
-
-RANDOM_PREFIX="$(get_code_verifier | cut -c1-6)"
-
-DEMO_PREFIX='cs_grpc_demo_py_'$RANDOM_PREFIX
-GRPC_SERVER_URL="$(echo "$GRPC_SERVER_URL" | sed 's@^.*/@@')"   # Remove leading tcp:// if any
-
-function api_curl()
-{
-  curl -s -o http_response.out -w '%{http_code}' "$@" > http_code.out
-  echo >> http_response.out
-  cat http_response.out
+  curl -s -D api_curl_http_header.out -o api_curl_http_response.out -w '%{http_code}' "$@" > api_curl_http_code.out
+  echo >> api_curl_http_response.out
+  cat api_curl_http_response.out
 }
 
 clean_up()
 {
-  #ACCESS_TOKEN="$(curl -s ${AB_BASE_URL}/iam/v3/oauth/token -H 'Content-Type: application/x-www-form-urlencoded' -u "$AB_CLIENT_ID:$AB_CLIENT_SECRET" -d "grant_type=client_credentials" | jq --raw-output .access_token)"
-
   echo Deleting player ...
-  curl -X DELETE "${AB_BASE_URL}/iam/v3/admin/namespaces/$AB_NAMESPACE/users/$USER_ID/information" -H "Authorization: Bearer $ACCESS_TOKEN"
+
+  api_curl -X DELETE "${AB_BASE_URL}/iam/v3/admin/namespaces/$AB_NAMESPACE/users/$USER_ID/information" -H "Authorization: Bearer $ACCESS_TOKEN"
 
   echo Resetting cloudsave validator ...
 
-  curl -X DELETE -s "${AB_BASE_URL}/cloudsave/v1/admin/namespaces/$AB_NAMESPACE/plugins" -H "Authorization: Bearer $ACCESS_TOKEN" -H 'Content-Type: application/json' >/dev/null
+  api_curl -X DELETE -s "${AB_BASE_URL}/cloudsave/v1/admin/namespaces/$AB_NAMESPACE/plugins" -H "Authorization: Bearer $ACCESS_TOKEN" -H 'Content-Type: application/json' >/dev/null
 }
 
 trap clean_up EXIT
 
 echo Logging in client ...
 
-ACCESS_TOKEN="$(curl -s ${AB_BASE_URL}/iam/v3/oauth/token -H 'Content-Type: application/x-www-form-urlencoded' -u "$AB_CLIENT_ID:$AB_CLIENT_SECRET" -d "grant_type=client_credentials" | jq --raw-output .access_token)"
+ACCESS_TOKEN="$(api_curl -s ${AB_BASE_URL}/iam/v3/oauth/token -H 'Content-Type: application/x-www-form-urlencoded' -u "$AB_CLIENT_ID:$AB_CLIENT_SECRET" -d "grant_type=client_credentials" | jq --raw-output .access_token)"
 
-echo Registering cloudsave validator $GRPC_SERVER_URL ...
-
-curl -X POST -s "${AB_BASE_URL}/cloudsave/v1/admin/namespaces/$AB_NAMESPACE/plugins" -H "Authorization: Bearer $ACCESS_TOKEN" -H 'Content-Type: application/json' -d "{\"customConfig\":{\"GRPCAddress\":\"${GRPC_SERVER_URL}\"},\"customFunction\":{\"afterReadGameRecord\":true,\"beforeWritePlayerRecord\":true},\"extendType\":\"CUSTOM\"}" >/dev/null
-
-echo Creating PLAYER ...
-
-USER_ID="$(api_curl -s "${AB_BASE_URL}/iam/v4/public/namespaces/$AB_NAMESPACE/users" -H "Authorization: Bearer $ACCESS_TOKEN" -H 'Content-Type: application/json' -d "{\"authType\":\"EMAILPASSWD\",\"country\":\"ID\",\"dateOfBirth\":\"1995-01-10\",\"displayName\":\"Cloudsave gRPC Player $RANDOM_PREFIX\",\"uniqueDisplayName\":\"Cloudsave gRPC Player $RANDOM_PREFIX\",\"emailAddress\":\"${DEMO_PREFIX}_player@test.com\",\"password\":\"GFPPlmdb2-\",\"username\":\"${DEMO_PREFIX}_player\"}" | jq --raw-output .userId)"
-
-if [ "$USER_ID" == "null" ]; then
-  cat http_response.out
+if [ "$(cat api_curl_http_code.out)" -ge "400" ]; then
+  cat api_curl_http_response.out
   exit 1
 fi
 
-echo Test BeforeWritePlayerRecord an VALID payload ...
+if [ -n "$GRPC_SERVER_URL" ]; then
+  echo Registering cloudsave validator $GRPC_SERVER_URL ...
+
+  api_curl -X POST -s "${AB_BASE_URL}/cloudsave/v1/admin/namespaces/$AB_NAMESPACE/plugins" -H "Authorization: Bearer $ACCESS_TOKEN" -H 'Content-Type: application/json' -d "{\"customConfig\":{\"GRPCAddress\":\"${GRPC_SERVER_URL}\"},\"customFunction\":{\"afterReadGameRecord\":true,\"beforeWritePlayerRecord\":true},\"extendType\":\"CUSTOM\"}" >/dev/null
+
+  if [ "$(cat api_curl_http_code.out)" -ge "400" ]; then
+    exit 1
+  fi
+elif [ -n "$EXTEND_APP_NAME" ]; then
+  echo Registering cloudsave validator $EXTEND_APP_NAME ...
+
+  api_curl -X POST -s "${AB_BASE_URL}/cloudsave/v1/admin/namespaces/$AB_NAMESPACE/plugins" -H "Authorization: Bearer $ACCESS_TOKEN" -H 'Content-Type: application/json' -d "{\"appConfig\":{\"appName\":\"${EXTEND_APP_NAME}\"},\"customFunction\":{\"afterReadGameRecord\":true,\"beforeWritePlayerRecord\":true},\"extendType\":\"APP\"}" >/dev/null
+
+  if [ "$(cat api_curl_http_code.out)" -ge "400" ]; then
+    exit 1
+  fi
+else
+  echo "GRPC_SERVER_URL or EXTEND_APP_NAME is not set"
+  exit 1
+fi
+
+echo Creating PLAYER ...
+
+USER_ID="$(api_curl -s "${AB_BASE_URL}/iam/v4/public/namespaces/$AB_NAMESPACE/users" -H "Authorization: Bearer $ACCESS_TOKEN" -H 'Content-Type: application/json' -d "{\"authType\":\"EMAILPASSWD\",\"country\":\"ID\",\"dateOfBirth\":\"1995-01-10\",\"displayName\":\"Cloudsave gRPC Player\",\"uniqueDisplayName\":\"Cloudsave gRPC Player\",\"emailAddress\":\"${DEMO_PREFIX}_player@test.com\",\"password\":\"GFPPlmdb2-\",\"username\":\"${DEMO_PREFIX}_player\"}" | jq --raw-output .userId)"
+
+if [ "$(cat api_curl_http_code.out)" -ge "400" ]; then
+  cat api_curl_http_response.out
+  exit 1
+fi
+
+echo Test BeforeWritePlayerRecord an VALID payload ... 
 
 api_curl -X PUT -s "${AB_BASE_URL}/cloudsave/v1/admin/namespaces/$AB_NAMESPACE/users/$USER_ID/records/favourite_weapon" -H "Authorization: Bearer $ACCESS_TOKEN" -H 'Content-Type: application/json' -d '{"userId": "1e076bcee6d14c849ffb121c0e0135be", "favouriteWeaponType": "SWORD", "favouriteWeapon": "excalibur"}'
 echo
 
-echo Test BeforeWritePlayerRecord an INVALID payload ...
+if [ "$(cat api_curl_http_code.out)" -ge "400" ]; then
+  exit 1
+fi
+
+echo Test BeforeWritePlayerRecord an INVALID payload ... 
 
 api_curl -X PUT -s "${AB_BASE_URL}/cloudsave/v1/admin/namespaces/$AB_NAMESPACE/users/$USER_ID/records/favourite_weapon" -H "Authorization: Bearer $ACCESS_TOKEN" -H 'Content-Type: application/json' -d '{"foo":"bar"}' || true
 echo
 
-# echo Logging in PLAYER ...
-
-# CODE_VERIFIER="$(get_code_verifier)"
-
-# REQUEST_ID="$(curl -s -D - "${AB_BASE_URL}/iam/v3/oauth/authorize?scope=commerce+account+social+publishing+analytics&response_type=code&code_challenge_method=S256&code_challenge=$(get_code_challenge "$CODE_VERIFIER")&client_id=$AB_CLIENT_ID" | grep -o 'request_id=[a-f0-9]\+' | cut -d= -f2)"
-
-# CODE="$(curl -s -D - ${AB_BASE_URL}/iam/v3/authenticate -H 'Content-Type: application/x-www-form-urlencoded' -d "password=GFPPlmdb2-&user_name=${DEMO_PREFIX}_player_$PLAYER_NUMBER@test.com&request_id=$REQUEST_ID&client_id=$AB_CLIENT_ID" | grep -o 'code=[a-f0-9]\+' | cut -d= -f2)"
-
-# PLAYER_TOKEN_RESPONSE="$(curl -s ${AB_BASE_URL}/iam/v3/oauth/token -H 'Content-Type: application/x-www-form-urlencoded' -u "$AB_CLIENT_ID:$AB_CLIENT_SECRET" -d "code=$CODE&grant_type=authorization_code&client_id=$AB_CLIENT_ID&code_verifier=$CODE_VERIFIER")"
-
-# PLAYER_ACCESS_TOKEN="$(echo "$PLAYER_TOKEN_RESPONSE" | jq --raw-output .access_token)"
-
-# echo Test AfterReadGameRecord an INVALID payload ...
-
-# curl -X PUT -s "${AB_BASE_URL}/cloudsave/v1/admin/namespaces/$AB_NAMESPACE/records/daily_msg" -H "Authorization: Bearer $ACCESS_TOKEN" -H 'Content-Type: application/json' -d '{"message":"mymsg","title":"mytitle","availableOn":"2023-08-13T00:00:00.000Z"}' >/dev/null
-
-# curl -s "${AB_BASE_URL}/cloudsave/v1/namespaces/$AB_NAMESPACE/records/daily_msg" -H "Authorization: Bearer $PLAYER_ACCESS_TOKEN" -H 'Content-Type: application/json' || true
+if [ "$(cat api_curl_http_code.out)" -lt "400" ]; then
+  exit 1
+fi
