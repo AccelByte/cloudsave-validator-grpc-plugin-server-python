a3c804ef052d2f603f576d3a62106ce547b7ff0d
Elmer Nocon <elmernocon@gmail.com>
2025-02-27 13:10:24 +0800

    fix(tracing): add http client instrumentation

--- src/accelbyte_grpc_plugin/utils.py
+++ src/accelbyte_grpc_plugin/utils.py
@@ -3,6 +3,11 @@
 # and restrictions contact your company contract manager.
 
 from environs import Env
+from logging import Logger
+from typing import Optional
+
+from accelbyte_py_sdk import AccelByteSDK
+from accelbyte_py_sdk.core import HttpxHttpClient, RequestsHttpClient
 
 
 def create_env(**kwargs) -> Env:
@@ -19,6 +24,33 @@ def create_env(**kwargs) -> Env:
     return env
 
 
+def instrument_sdk_http_client(sdk: AccelByteSDK, logger: Optional[Logger] = None) -> None:
+    http_client = sdk.get_http_client(raise_when_none=False)
+    if http_client is not None:
+        if isinstance(http_client, HttpxHttpClient):
+            try:
+                from opentelemetry.instrumentation.httpx import HTTPXClientInstrumentor
+                HTTPXClientInstrumentor().instrument()
+                if logger:
+                    logger.info("httpx client instrumented")
+            except ImportError:
+                if logger:
+                    logger.warning("failed to instrument httpx client")
+        elif isinstance(http_client, RequestsHttpClient):
+            try:
+                from opentelemetry.instrumentation.requests import RequestsInstrumentor
+                RequestsInstrumentor().instrument()
+                if logger:
+                    logger.info("requests client instrumented")
+            except ImportError:
+                if logger:
+                    logger.warning("failed to instrument requests client")
+        else:
+            if logger:
+                logger.warning("failed to instrument unknown client")
+
+
 __all__ = [
     "create_env",
+    "instrument_sdk_http_client",
 ]
--- src/app/__main__.py
+++ src/app/__main__.py
@@ -1,11 +1,20 @@
-# Copyright (c) 2023 AccelByte Inc. All Rights Reserved.
+# Copyright (c) 2025 AccelByte Inc. All Rights Reserved.
 # This is licensed software from AccelByte Inc, for limitations
 # and restrictions contact your company contract manager.
 
 import asyncio
 import logging
+
 from logging import Logger
-from typing import List, Optional
+from typing import List
+
+from accelbyte_py_sdk.core import (
+    AccelByteSDK,
+    DictConfigRepository,
+    InMemoryTokenRepository,
+    HttpxHttpClient,
+)
+from accelbyte_py_sdk.services import auth as auth_service
 
 from environs import Env
 
@@ -15,43 +24,60 @@ from accelbyte_grpc_plugin.app import (
     AppOptionGRPCInterceptor,
     AppOptionGRPCService,
 )
-from accelbyte_grpc_plugin.utils import create_env
+from accelbyte_grpc_plugin.utils import instrument_sdk_http_client
 
-from .proto.cloudsaveValidatorService_pb2_grpc import (
-    add_CloudsaveValidatorServiceServicer_to_server,
-)
-from .services.cloudsave_validator_service import (
-    AsyncCloudsaveValidatorService,
-)
+from .proto.cloudsaveValidatorService_pb2_grpc import add_CloudsaveValidatorServiceServicer_to_server
+
+from .services.cloudsave_validator_service import AsyncCloudsaveValidatorService
+from .utils import create_env
 
 DEFAULT_APP_PORT: int = 6565
 
 DEFAULT_AB_BASE_URL: str = "https://test.accelbyte.io"
 DEFAULT_AB_NAMESPACE: str = "accelbyte"
-DEFAULT_AB_SECURITY_CLIENT_ID: Optional[str] = None
-DEFAULT_AB_SECURITY_CLIENT_SECRET: Optional[str] = None
 
 DEFAULT_ENABLE_HEALTH_CHECK: bool = True
 DEFAULT_ENABLE_PROMETHEUS: bool = True
 DEFAULT_ENABLE_REFLECTION: bool = True
 DEFAULT_ENABLE_ZIPKIN: bool = True
 
-DEFAULT_PLUGIN_GRPC_SERVER_AUTH_ENABLED: bool = False
-
+DEFAULT_PLUGIN_GRPC_SERVER_AUTH_ENABLED: bool = True
 DEFAULT_PLUGIN_GRPC_SERVER_LOGGING_ENABLED: bool = False
 DEFAULT_PLUGIN_GRPC_SERVER_METRICS_ENABLED: bool = True
 
 
 async def main(**kwargs) -> None:
-    logger = logging.getLogger("app")
-    logger.setLevel(logging.INFO)
-    logger.addHandler(logging.StreamHandler())
-
     env = create_env(**kwargs)
 
     port: int = env.int("PORT", DEFAULT_APP_PORT)
 
-    options = create_options(env=env, logger=logger)
+    logger = logging.getLogger("app")
+    logger.setLevel(logging.INFO)
+    logger.addHandler(logging.StreamHandler())
+
+    config = DictConfigRepository(dict(env.dump()))
+    token = InMemoryTokenRepository()
+    http = HttpxHttpClient()
+    http.client.follow_redirects = True
+
+    sdk = AccelByteSDK()
+    sdk.initialize(
+        options={
+            "config": config,
+            "token": token,
+            "http": http,
+        }
+    )
+
+    instrument_sdk_http_client(sdk=sdk, logger=logger)
+
+    _, error = await auth_service.login_client_async(sdk=sdk)
+    if error:
+        raise Exception(str(error))
+
+    sdk.timer = auth_service.LoginClientTimer(2880, repeats=-1, autostart=True, sdk=sdk)
+
+    options = create_options(sdk=sdk, env=env, logger=logger)
     options.append(
         AppOptionGRPCService(
             full_name=AsyncCloudsaveValidatorService.full_name,
@@ -64,16 +90,11 @@ async def main(**kwargs) -> None:
     await app.run()
 
 
-def create_options(env: Env, logger: Logger) -> List[AppOption]:
+def create_options(sdk: AccelByteSDK, env: Env, logger: Logger) -> List[AppOption]:
     options: List[AppOption] = []
 
     with env.prefixed("AB_"):
-        base_url = env.str("BASE_URL", DEFAULT_AB_BASE_URL)
         namespace = env.str("NAMESPACE", DEFAULT_AB_NAMESPACE)
-        client_id = env.str("CLIENT_ID", DEFAULT_AB_SECURITY_CLIENT_ID)
-        client_secret = env.str(
-            "CLIENT_SECRET", DEFAULT_AB_SECURITY_CLIENT_SECRET
-        )
 
     with env.prefixed("ENABLE_"):
         if env.bool("HEALTH_CHECK", DEFAULT_ENABLE_HEALTH_CHECK):
@@ -100,25 +121,12 @@ def create_options(env: Env, logger: Logger) -> List[AppOption]:
     with env.prefixed("PLUGIN_GRPC_SERVER_"):
         with env.prefixed("AUTH_"):
             if env.bool("ENABLED", DEFAULT_PLUGIN_GRPC_SERVER_AUTH_ENABLED):
-                from accelbyte_py_sdk import AccelByteSDK
-                from accelbyte_py_sdk.core import MyConfigRepository, InMemoryTokenRepository
                 from accelbyte_py_sdk.token_validation.caching import CachingTokenValidator
-                from accelbyte_py_sdk.services.auth import login_client, LoginClientTimer
                 from accelbyte_grpc_plugin.interceptors.authorization import AuthorizationServerInterceptor
 
-                config = MyConfigRepository(base_url, client_id, client_secret, namespace)
-                token = InMemoryTokenRepository()
-                sdk = AccelByteSDK()
-                sdk.initialize(options={"config": config, "token": token})
-                result, error = login_client(sdk=sdk)
-                if error:
-                    raise Exception(str(error))
-                sdk.timer = LoginClientTimer(2880, repeats=-1, autostart=True, sdk=sdk)
                 options.append(
                     AppOptionGRPCInterceptor(
                         interceptor=AuthorizationServerInterceptor(
-                            resource=env.str("RESOURCE", None),
-                            action=env.int("ACTION", None),
                             namespace=namespace,
                             token_validator=CachingTokenValidator(sdk=sdk),
                         )
@@ -147,5 +155,9 @@ def create_options(env: Env, logger: Logger) -> List[AppOption]:
     return options
 
 
-if __name__ == "__main__":
+def run() -> None:
     asyncio.run(main())
+
+
+if __name__ == "__main__":
+    run()
